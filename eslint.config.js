488c0ec53d1742a6d104481749a5326d5b7b2f55
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-04-18 14:34:04 -0300

    feat: enhance ESLint configuration with stylistic rules and plugins

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,16 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      '@stylistic': stylistic,
+      'react-hooks': reactHooks,
+    },
     extends: [
       js.configs.recommended,
 
@@ -27,6 +32,16 @@ export default defineConfig([
       },
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/semi': ['error', 'always'],
+      "react-hooks/exhaustive-deps": 'warn',
     },
   },
 ])
