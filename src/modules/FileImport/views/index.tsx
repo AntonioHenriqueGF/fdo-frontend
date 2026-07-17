2a94df52119d289f1453f2bd8b8b0ee8e0014aae
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-07-17 18:47:47 -0300

    feat: conditionally render ImportPanelSelectionWrapper based on fileImportHash state

--- src/modules/FileImport/views/index.tsx
+++ src/modules/FileImport/views/index.tsx
@@ -3,12 +3,13 @@ import CSVReader from '../components/CSVReader';
 import { PreviewGrid } from '../components/PreviewGrid';
 import { UserCustomInput } from '../components/UserCustomInput';
 import { ImportPanelSelectionWrapper } from './styles';
-import { deleteFileImportAtom } from '../atoms/importAtoms';
+import { deleteFileImportAtom, fileImportHashAtom } from '../atoms/importAtoms';
 import { useAtom } from 'jotai';
 import { ContentPad } from '../../../shared/components/ContentPad';
 
 export const FileImportView: React.FC = () => {
   const [, deleteFileImport] = useAtom(deleteFileImportAtom);
+  const [fileImportHash] = useAtom(fileImportHashAtom);
   useEffect(() => {
     return () => {
       deleteFileImport();
@@ -19,10 +20,16 @@ export const FileImportView: React.FC = () => {
     <ContentPad>
       <h2>File Import</h2>
       <CSVReader />
-      <ImportPanelSelectionWrapper>
-        <UserCustomInput />
-        <PreviewGrid />
-      </ImportPanelSelectionWrapper>
+      {
+        fileImportHash ? (
+          <ImportPanelSelectionWrapper>
+            <UserCustomInput />
+            <PreviewGrid />
+          </ImportPanelSelectionWrapper>
+        ) : (
+          <></>
+        )
+      }
     </ContentPad>
   );
 };
