8a367091d30d249625a59668116c1435f2f51ddd
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-07-17 23:12:56 -0300

    refactor: remove unused ApiRequest and useEffect in GlobalGuard component

--- src/modules/GlobalGuard/index.tsx
+++ src/modules/GlobalGuard/index.tsx
@@ -1,14 +1,5 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router';
-import { ApiRequest } from '../../Services/ApiRequest';
 
 export const GlobalGuard: React.FC = () => {
-  useEffect(() => {
-    ApiRequest({
-      url: '/api/erro',
-      method: 'GET',
-    });
-  }, []);
-
   return <Outlet />;
 };
