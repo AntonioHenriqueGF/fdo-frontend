17385c6434f926c2b6efe35482f55c9a0ed8fd03
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-08-11 23:09:57 -0300

    feat: implement GlobalGuard to handle user authentication and navigation

--- src/modules/GlobalGuard/index.tsx
+++ src/modules/GlobalGuard/index.tsx
@@ -1,5 +1,29 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router';
+import {
+  ApiRequest,
+  type StandardApiResponse,
+} from '../../Services/ApiRequest';
 
 export const GlobalGuard: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Checks only if the path is '/'. For other paths, the verification is handled by AuthGuard and UnauthGuard.
+    if (window.location.pathname !== '/') {
+      return;
+    }
+    ApiRequest<StandardApiResponse>({
+      method: 'GET',
+      url: '/api/me',
+      callback: () => {
+        navigate('/dashboard', { replace: true });
+      },
+      errorCallback: () => {
+        navigate('/login', { replace: true });
+      },
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return <Outlet />;
 };
