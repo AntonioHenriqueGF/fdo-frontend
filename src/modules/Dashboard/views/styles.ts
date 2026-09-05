196ccaabd7bfef3f8122ad2e4c084ec43fd0a2f4
AntonioHenriqueGF <hgfantonio@gmail.com>
2026-09-05 11:16:15 -0300

    style: improve formatting consistency in HeaderWrapper and ContentWrapper styles

--- src/modules/Dashboard/views/styles.ts
+++ src/modules/Dashboard/views/styles.ts
@@ -2,63 +2,69 @@ import type { CSSProperties } from '@mui/material';
 import styled from 'styled-components';
 
 export const HeaderWrapper = styled.header`
-    display: flex;
-    align-items: center;
-    gap: 20px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  margin: 10px;
+
+  * {
+    font-family: var(--standard-font-family);
+  }
+
+  .drawer-button {
+    padding: 10px;
+    color: var(--standard-text-color);
+    &:hover {
+      background-color: var(--hover-background-color);
+      color: var(--hover-text-color);
+    }
+  }
+
+  h1 {
+    font-size: 1.5rem;
+    color: var(--standard-text-color);
+    margin: 10px 0;
+  }
+
+  .logout-button {
+    position: absolute;
+    right: 10px;
     margin: 10px;
 
-    * {
-        font-family: var(--standard-font-family);
+    font-weight: bold;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    text-align: center;
+    gap: 10px;
+
+    .logo-icon {
+      width: 60px;
+      height: 60px;
     }
 
     .drawer-button {
-        padding: 10px;
-        color: var(--standard-text-color);
-        &:hover {
-            background-color: var(--hover-background-color);
-            color: var(--hover-text-color);
-        }
+      position: absolute;
+      left: 10px;
     }
-
-    h1 {
-        font-size: 1.5rem;
-        color: var(--standard-text-color);
-        margin: 10px 0;
-    }
-
-    .logout-button {
-        position: absolute;
-        right: 10px;
-        margin: 10px;
-
-        font-weight: bold;
-    }
-
-    @media (max-width: 600px) {
-        flex-direction: column;
-        text-align: center;
-        gap: 10px;
-
-        .logo-icon {
-            width: 60px;
-            height: 60px;
-        }
-
-        .drawer-button {
-            position: absolute;
-            left: 10px;
-        }
-    }
+  }
 `;
 
 export const ContentWrapper = styled.div`
-    display: flex;
+  display: flex;
 
+  .page-content {
+    margin: 10px;
+    width: 100%;
+    height: calc(100vh - 89px);
+  }
+
+  @media (max-width: 600px) {
     .page-content {
-        margin: 10px;
-        width: 100%;
-        height: calc(100vh - 89px);
+      height: calc(100vh - 158px);
     }
+  }
 `;
 
 export const headerStyles = {
